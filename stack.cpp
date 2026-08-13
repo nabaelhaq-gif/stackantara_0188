fab368d1fe203a8cba387c374432fcdd0bfff22b
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-13 10:25:37 +0700

    inisialisasi variabel data dan node

--- stack.cpp
+++ stack.cpp
@@ -4,4 +4,9 @@ using namespace std;
 //1. inisialisasi class node
 class Node {
 public:
-    
+    //2. inisialisasi variabel data dan Node next sebagai member class Node
+    int data;
+    Node* next;
+
+    Node() {
+        
