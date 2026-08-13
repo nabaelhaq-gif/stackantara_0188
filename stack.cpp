8064ea72759ab2c0fd0f19abbc860dd2973a5a29
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-13 10:29:14 +0700

    membuat prosedur pop

--- stack.cpp
+++ stack.cpp
@@ -37,4 +37,14 @@ public:
         return value;
     }
 
+    //8. pembuatan prosedur pop
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is Empty" << endl;
+        }
+
+        cout << "Popped Value: " << top->data << endl;
+        top = top->next;
+    }
+
     
