df5ac1e921fad8dd99fa96dabf365142726ad6c3
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-13 10:28:43 +0700

    membuat method push

--- stack.cpp
+++ stack.cpp
@@ -27,4 +27,14 @@ public:
         top = NULL;
     }
 
+    //7. pembuatan method push
+    int push(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
+
     
